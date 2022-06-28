fe1bf1eeb7c990c33426f6656e78306aa87a08bf
thaian161 <thaian161@yahoo.com>
2022-06-28 15:12:21 -0500

    mistakes in CREATE TABLE -> re-created tables

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -1,20 +1,5 @@
 CREATE TABLE assignments (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  content TEXT,
-  day INTEGER,
-  chapter INTEGER,,
-  duration INTEGER,,
-);
-
-CREATE TABLE assignment_submissions (
-  id SERIAL PRIMARY KEY NOT NULL,
-  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
-  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
-  duration ,
-  submission_date DATE,
-);CREATE TABLE assignments (
-  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   content TEXT,
   day INTEGER,
